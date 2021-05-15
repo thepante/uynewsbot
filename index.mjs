4d9c8d52e0d4beda373789d0f763816c87064710
pante <panterua@hotmail.com>
2021-05-15 00:38:02 -0300

    basic changes
    env var & init subreddits (testing)

--- index.mjs
+++ index.mjs
@@ -1,48 +1,46 @@
-import Snoowrap from 'snoowrap';
-import SnooStorm from 'snoostorm';
 import nconf from 'nconf';
-
-global.__basedir = process.cwd();
-nconf.file('conf', '.configuration.json' )
-    .file('credentials', '.credentials.json');
+import SnooStorm from 'snoostorm';
+import Snoowrap from 'snoowrap';
 
 import processRedditPost from './src/processRedditPost.mjs';
 
+global.__basedir = process.cwd();
+nconf.file('conf', '.configuration.json');
+
+console.log('Env:', process.env.ENVIRONMENT);
+
 const snoowrap = new Snoowrap({
-    userAgent: nconf.get('bot:userAgent'),
-    clientId: nconf.get('credentials:clientId') || process.env.clientId,
-    clientSecret: nconf.get('credentials:clientSecret') || process.env.clientSecret,
-    username: nconf.get('credentials:redditUser') || process.env.redditUser,
-    password: nconf.get('credentials:redditPass') || process.env.redditPass,
+     userAgent: nconf.get('bot:userAgent'),
+      clientId: process.env.CLIENTID,
+  clientSecret: process.env.CLIENTSECRET,
+      username: process.env.REDDITUSER,
+      password: process.env.REDDITPASS,
 });
 
 function registerClient(clientConfig) {
-    console.log('Registering', clientConfig);
-    const isTestMode = process.env.environment === 'test';
-    const client = new SnooStorm.SubmissionStream(snoowrap, {
-        subreddit: clientConfig.id,
-        limit: clientConfig.limit,
-        pollTime: isTestMode ? 30000 : clientConfig.frequencyInMs
-    });
-    client.on('item', processRedditPost);
+  console.log('Registering', clientConfig);
+  const isTestMode = process.env.ENVIRONMENT === 'test';
+  const client = new SnooStorm.SubmissionStream(snoowrap, {
+    subreddit: clientConfig.id,
+    limit: clientConfig.limit,
+    pollTime: isTestMode ? 30000 : clientConfig.frequencyInMs
+  });
+  client.on('item', processRedditPost);
 }
 
 async function registerClients() {
-    const clients = nconf.get('subreddits');
-    const isTestMode = process.env.environment === 'test';
-    const finalClients = isTestMode ? clients.filter(val => val.testmode === true) : clients;
-    const waitTimeBetweenRegistrations = 60000 / finalClients.length;
+  const clients = nconf.get('subreddits');
+  const isTestMode = process.env.ENVIRONMENT === 'test';
+  const finalClients = isTestMode ? clients.filter(val => val.testmode === true) : clients;
+  const waitTimeBetweenRegistrations = 60000 / finalClients.length;
 
-    for (const client of finalClients) {
-        registerClient(client);
-        if (!isTestMode) {
-            await new Promise(resolve => setTimeout(resolve, waitTimeBetweenRegistrations));
-        }
+  for (const client of finalClients) {
+    registerClient(client);
+    if (!isTestMode) {
+      await new Promise(resolve => setTimeout(resolve, waitTimeBetweenRegistrations));
     }
+  }
 }
 
-
 registerClients();
-
-
 
