9254f7b732c2bc5dec1c7cf3c91671aaebdd99b9
pante <panterua@hotmail.com>
2021-05-18 15:22:23 -0300

    updated 180 name/altName

--- src/PageParser/180comuy.mjs
+++ src/PageParser/180comuy.mjs
@@ -1,7 +1,8 @@
 import PageParserBase from "./PageParserBase.mjs";
 
 export default class Cientoochentacomuy extends PageParserBase {
-    static name =  '180';
+    static name = 'Portal 180';
+    static altName = 'Grupo 180';
     static domainMatcher = [
         '180.com.uy'
     ]
