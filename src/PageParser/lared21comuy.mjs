99d49ceb20c3652eb19e538dd78648d6764982a2
pante <panterua@hotmail.com>
2021-05-20 21:01:18 -0300

    added alt name for lr21.com.uy

--- src/PageParser/lared21comuy.mjs
+++ src/PageParser/lared21comuy.mjs
@@ -3,6 +3,7 @@ import jQuery from "jquery";
 
 export default class Lared21comuy extends PageParserBase {
     static name =  'La Red 21';
+    static altName = 'LARED21';
     static domainMatcher = [
         'lr21.com.uy'
     ]
