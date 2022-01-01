ba8b6ba76b78a44c6e113864b7fb497c39ad44e5
thepante <panterua@hotmail.com>
2022-01-01 18:43:19 -0300

    improved logs in api actions

--- src/requestActions.mjs
+++ src/requestActions.mjs
@@ -1,38 +1,41 @@
 import processRedditPost from './processRedditPost.mjs';
 
+function log(id, subreddit, msg, details=false) {
+  console.log(`[${id}] [${subreddit}] ${msg}${details ? (' ⇢ ' + details) : ''}`);
+}
+
 export async function scanSubmission(req, res, snoowrap) {
     try {
         const { secret, subreddit, id } = req.headers;
 
         if (!secret || !subreddit || !id) {
-            console.log('Manual scan request → invalid headers');
+            log('API', '******', 'Manual scan request', 'Invalid headers');
             return res.sendStatus(400);
         };
 
         if (id.length !== 6) {
-            console.log('Manual scan request → invalid id:', id);
+            log(id, subreddit, 'Manual scan request', 'Invalid submission ID');
             return res.sendStatus(400);
         }
 
         if (secret === process.env.CLIENTSECRET) {
-            console.log('Manual scan request →', subreddit, id);
+            log(id, subreddit, 'Manual scan request');
             const submission = await snoowrap.getSubmission(id).fetch();
             const r = submission.subreddit.display_name;
             if (subreddit !== r) {
-                console.log('Manual scan request → subreddit does not match:', subreddit, id);
+                log(id, subreddit, 'Request error', 'Subreddit does not match');
                 return res.sendStatus(400);
             }
             await processRedditPost(submission, true);
             return res.sendStatus(200);
         } else {
             const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
-            console.log('Manual scan request auth failed from ip:', ip);
+            log(id, subreddit, 'Manual scan request auth failed from IP', ip);
             return res.sendStatus(403);
         }
 
     } catch(err) {
-        console.error('Error [API] →', err);
+        console.error('[API] Error ⇢', err);
         return res.sendStatus(500);
     }
 }
-
