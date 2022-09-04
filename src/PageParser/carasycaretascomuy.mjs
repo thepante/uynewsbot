0aefc542ba7041444de98e753870eb4c3dc2065c
thepante <panterua@hotmail.com>
2022-09-04 19:37:15 -0300

    updated carasycaretascomuy selectors

--- src/PageParser/carasycaretascomuy.mjs
+++ src/PageParser/carasycaretascomuy.mjs
@@ -4,7 +4,16 @@ export default class Carasycaretascomuy extends PageParserBase {
     static name = 'Caras y Caretas';
     static domainMatcher = [
         'carasycaretas.com.uy'
-    ]
+    ];
+    selectorsToRemove = [
+        '.news-stopper',
+        '.content_subscription',
+        '.related-note-in-body',
+        '.related-note-in-body-article',
+        '.article-figure',
+        '.thumb-wrap',
+        '.section-newsletter',
+    ];
     checkPaywalJSDOM(data) {
         const dom = data.window.document;
         const sel = '.entry-content .boton-comunidad a[href$="registro-socios/"]';
